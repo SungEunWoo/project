d776861c0bb46b1ec7c8e084d6281ae9116fe95d
SungEunWoo <sungeu18@naver.com>
2024-08-03 03:48:33 +0900

    controller_Basic

--- src/main/java/com/project_sew/project/MyController.java
+++ src/main/java/com/project_sew/project/MyController.java
@@ -1,5 +1,13 @@
 package com.project_sew.project;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MyController {
+    @GetMapping("/")
+    public String getMethodName() {
+        return "index";
+    }
     
 }
