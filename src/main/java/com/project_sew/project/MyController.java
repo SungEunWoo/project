e37e77eb1b259f3daf256013ffc0ffc0ed0af674
SungEunWoo <sungeu18@naver.com>
2024-08-19 15:37:07 +0900

    commit

--- src/main/java/com/project_sew/project/MyController.java
+++ src/main/java/com/project_sew/project/MyController.java
@@ -2,7 +2,6 @@ package com.project_sew.project;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import com.project_sew.project.jdbc.DAO.MyUserDAO_inter;
 
